6f3b86c3d1eddc5b39c195c4293bdb576e9a8131
FASALGAFOOR <fasalgafoor2080@gmail.com>
2025-11-01 14:56:11 +0530

    cart add ,update quantity

--- src/features/cart/cartController.js
+++ src/features/cart/cartController.js
@@ -1,14 +1,52 @@
 const User=require('../user/userModel')
 const product=require('../products/productModel')
-const Cart=require('../cart/cartModel')
+const cartModel = require('../cart/cartModel');
 
 exports.addtocart=async (req,res)=>{
     console.log("entered");
     
     try {
-        const {userid,productid,quantity}=req.body
-        console.log(req.body,"m");
+        const {userId,productId,quantity}=req.body
+        console.log(userId,"u");
+        console.log(productId,"p");
+        console.log(quantity,"q");
         
+        
+        const findproduct=await product.findById({_id:productId})
+        console.log(findproduct.actualPrice,"findproduct");
+        
+        if(!findproduct){
+            return res.status(404).json({message:'product not found'})
+        }else{
+            const checkcart =await cartModel.findOne({userId,productId})
+            console.log(checkcart,"checkcart");
+            if(checkcart){
+                checkcart.quantity =checkcart.quantity + 1
+                checkcart.totalPrice=checkcart.quantity * checkcart.price
+                await checkcart.save()
+                res.json({message:"quantity updated",checkcart})               
+            }else{
+                const price = findproduct.actualPrice;
+                console.log(price,"price");
+                
+                const newproduct=new cartModel({
+                    userId,
+                    productId,
+                    price,
+                    quantity:1,
+                    totalPrice:1*price
+                })
+                console.log(newproduct,'newproduct');
+                
+                await newproduct.save()
+                return res.status(200).json({message:"product added succefully"})
+            }
+            
+
+        }
+
+        
+
 
         
         
